2f5cc7768265b390e43fee7b0b2fee29f2bd389a
sam <ssgarretson@crimson.ua.edu>
2019-03-29 14:34:33 -0500

    add ship and bullets

---
+++ settings.py
@@ -0,0 +1,19 @@
+class Settings():
+    """A class to store all the settings for Alien Invasion"""
+
+    def __init__(self):
+        """Initialize the game's settings"""
+        # Screen Settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+
+        # Ship Settings
+        self.ship_speed_factor = 60.5
+
+        # Bullet Settings
+        self.bullet_speed_factor = 60
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 3
