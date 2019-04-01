8487b80e7aabe374732c0ac25714169496eb5424
sam <ssgarretson@crimson.ua.edu>
2019-04-01 14:12:47 -0500

    complete first iteration scoring system

--- ship.py
+++ ship.py
@@ -1,9 +1,11 @@
 import pygame
+from pygame.sprite import Sprite
 
-class Ship():
+class Ship(Sprite):
 
     def __init__(self, ai_settings, screen):
         """Initialize the ship and set its starting position."""
+        super(Ship, self).__init__()
         self.screen = screen
         self.ai_settings = ai_settings
 
@@ -42,6 +44,7 @@ class Ship():
 
         # Update rect object from self.center
         self.rect.centerx = self.center
+    
 
     def blitme(self):
         """Draw the ship at its current location"""
