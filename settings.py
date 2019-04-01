2f8ec51de674abd776254c2ba92f76d2c4570656
sam <ssgarretson@crimson.ua.edu>
2019-04-01 11:20:18 -0500

    add movement and collisions

--- settings.py
+++ settings.py
@@ -2,18 +2,40 @@ class Settings():
     """A class to store all the settings for Alien Invasion"""
 
     def __init__(self):
-        """Initialize the game's settings"""
+        """Initialize the game's static settings"""
         # Screen Settings
-        self.screen_width = 1200
+        self.screen_width = 1218
         self.screen_height = 800
-        self.bg_color = (230, 230, 230)
+        self.bg_color = (0, 0, 0)
 
         # Ship Settings
-        self.ship_speed_factor = 60.5
+        self.ship_limit = 3
 
         # Bullet Settings
-        self.bullet_speed_factor = 60
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet_color = 60, 60, 60
+        self.bullet_color = 255, 50, 50
         self.bullets_allowed = 3
+
+        # Alien Settings
+        self.fleet_drop_speed = 50
+
+        # How quickly the gem speeds up
+        self.speedup_scale = 1.1
+
+        self.initialize_dynamic_settings()
+    
+    def initialize_dynamic_settings(self):
+        """Initialize the game's dynamic settings"""
+        self.ship_speed_factor = 60.5
+        self.bullet_speed_factor = 60
+        self.alien_speed_factor = 5
+
+        # fleet_direction of 1 represents right; -1 represents left
+        self.fleet_direction = 1
+
+    def increase_speed(self):
+        """Increase speed settings"""
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
